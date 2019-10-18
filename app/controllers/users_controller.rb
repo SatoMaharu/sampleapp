11631a2c17a3d7d56f67414e1523d0ae9e413f73
SatoMaharu <fenc.m01@gmail.com>
2019-10-18 05:57:52 +0000

    save222

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end 
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザー情報が更新されませんでした。"
+      render :edit
+    end 
+  end 
   
   def show
     @user = User.find(params[:id])
@@ -18,6 +37,12 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
+  end 
   
   private
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,26 @@
 User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
+              
+100.times do |n|
+  name = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+3.times do |user_id|
+  user_id += 1
+  50.times do |n|
+    name = "サンプル#{n+1}"
+    note = "サンプルです。"
+    Task.create!(name: name,
+                 note: note,
+                 user_id: user_id)
+  end 
+end 
